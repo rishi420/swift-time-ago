f08bcce1019bb623bcf080c6c598e93e3db60122
Warif Akhand Rishi <rishi420@gmail.com>
2019-04-23 13:39:49 +0600

    Project compiled with xcode 10.2

--- Swift Time Ago/Swift Time Ago/ViewController.swift
+++ Swift Time Ago/Swift Time Ago/ViewController.swift
@@ -9,5 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(DateTimeFormattedAsTimeAgo.displayDate(date: Date()))
+    }
 }
 
