e33fedeb5b682bd375c0022410b21c40deafd9ed
Warif Akhand Rishi <rishi420@gmail.com>
2019-04-23 13:34:33 +0600

    Add files via upload

--- Swift Time Ago/Swift Time Ago/DateTimeFormattedAsTimeAgo.swift
+++ Swift Time Ago/Swift Time Ago/DateTimeFormattedAsTimeAgo.swift
@@ -8,113 +8,124 @@
 
 import Foundation
 
-public func dateTimeFormattedAsTimeAgo(date: NSDate, referenceDate now: NSDate = NSDate()) -> String {
-
-  let secondsFromDate = now.secondsFrom(date)
-  if secondsFromDate < 60 {
-    return secondsFormatter()
-  }
-  
-  let minutesFromDate = now.minutesFrom(date)
-  if minutesFromDate < 60 {
-    return minutesFormatter(minutesFromDate)
-  }
-  
-  let hoursFromDate = now.hoursFrom(date)
-  if hoursFromDate < 24 {
-    return hoursFormatter(hoursFromDate)
-  }
-  
-  let daysFromDate = now.daysFrom(date)
-  switch daysFromDate {
-  case 1:
-    return yesterdayFormatter()
-  case 2...6:
-    return daysFormatter(daysFromDate)
-  default:
-    break
-  }
-  
-  let weeksFromDate = now.weeksFrom(date)
-  let monthsFromDate = now.monthsFrom(date)
-  switch monthsFromDate {
-  case 0:
-    return weeksFormatter(weeksFromDate)
-  case 1...11:
-    return monthsFormatter(monthsFromDate)
-  default:
-    break
-  }
-  
-  let yearsFromDate = now.yearsFrom(date)
-  return yearsFormatter(yearsFromDate)
+class DateTimeFormattedAsTimeAgo {
+    
+    public class func displayDate(date: Date, referenceDate now: Date = Date()) -> String {
+        
+        let secondsFromDate = now.secondsFrom(date: date)
+        if secondsFromDate < 60 {
+            return secondsFormatter()
+        }
+        
+        let minutesFromDate = now.minutesFrom(date: date)
+        if minutesFromDate < 60 {
+            return minutesFormatter(minutes: minutesFromDate)
+        }
+        
+        let hoursFromDate = now.hoursFrom(date: date)
+        if hoursFromDate < 24 {
+            return hoursFormatter(hours: hoursFromDate)
+        }
+        
+        let daysFromDate = now.daysFrom(date: date)
+        switch daysFromDate {
+        case 1:
+            return yesterdayFormatter()
+        case 2...6:
+            return daysFormatter(days: daysFromDate)
+        default:
+            break
+        }
+        
+        let weeksFromDate = now.weeksFrom(date: date)
+        let monthsFromDate = now.monthsFrom(date: date)
+        switch monthsFromDate {
+        case 0:
+            return weeksFormatter(weeks: weeksFromDate)
+        case 1...11:
+            return monthsFormatter(months: monthsFromDate)
+        default:
+            break
+        }
+        
+        let yearsFromDate = now.yearsFrom(date: date)
+        return yearsFormatter(years: yearsFromDate)
+    }
 }
 
-// MARK: Formatter functions
-
-func classicFormatterAgo(quantity: Int, unit: String) -> String {
-  var formattedString = "\(quantity) \(unit)"
-  if quantity > 1 {
-    formattedString += "s"
-  }
-  formattedString += " ago"
-  return formattedString
+// MARK: Formatter Private Functions
+extension DateTimeFormattedAsTimeAgo {
+    
+    private class func classicFormatterAgo(quantity: Int, unit: String) -> String {
+        var formattedString = "\(quantity) \(unit)"
+        if quantity > 1 {
+            formattedString += "s"
+        }
+        formattedString += " ago"
+        return formattedString
+    }
+    
+    private class func secondsFormatter() -> String {
+        return "Just now"
+    }
+    
+    private class func minutesFormatter(minutes: Int) -> String {
+        return classicFormatterAgo(quantity: minutes, unit: "minute")
+    }
+    
+    private class func hoursFormatter(hours: Int) -> String {
+        return classicFormatterAgo(quantity: hours, unit: "hour")
+    }
+    
+    private class func yesterdayFormatter() -> String {
+        return "Yesterday"
+    }
+    
+    private class func daysFormatter(days: Int) -> String {
+        return classicFormatterAgo(quantity: days, unit: "day")
+    }
+    
+    private class func weeksFormatter(weeks: Int) -> String {
+        return classicFormatterAgo(quantity: weeks, unit: "week")
+    }
+    
+    private class func monthsFormatter(months: Int) -> String {
+        return classicFormatterAgo(quantity: months, unit: "month")
+    }
+    
+    private class func yearsFormatter(years: Int) -> String {
+        return classicFormatterAgo(quantity: years, unit: "year")
+    }
 }
 
-func secondsFormatter() -> String {
-  return "Just now"
-}
-
-func minutesFormatter(minutes: Int) -> String {
-  return classicFormatterAgo(minutes, "minute")
-}
-
-func hoursFormatter(hours: Int) -> String {
-  return classicFormatterAgo(hours, "hour")
-}
-
-func yesterdayFormatter() -> String {
-  return "Yesterday"
-}
-
-func daysFormatter(days: Int) -> String {
-  return classicFormatterAgo(days, "day")
-}
-
-func weeksFormatter(weeks: Int) -> String {
-  return classicFormatterAgo(weeks, "week")
-}
-
-func monthsFormatter(months: Int) -> String {
-  return classicFormatterAgo(months, "month")
-}
-
-func yearsFormatter(years: Int) -> String {
-  return classicFormatterAgo(years, "year")
-}
-
-// MARK: Extension of NSDate
-
-private extension NSDate {
-  func yearsFrom(date:NSDate) -> Int{
-    return NSCalendar.currentCalendar().components(NSCalendarUnit.CalendarUnitYear, fromDate: date, toDate: self, options: nil).year
-  }
-  func monthsFrom(date:NSDate) -> Int{
-    return NSCalendar.currentCalendar().components(NSCalendarUnit.CalendarUnitMonth, fromDate: date, toDate: self, options: nil).month
-  }
-  func weeksFrom(date:NSDate) -> Int{
-    return NSCalendar.currentCalendar().components(NSCalendarUnit.CalendarUnitWeekOfYear, fromDate: date, toDate: self, options: nil).weekOfYear
-  }
-  func daysFrom(date:NSDate) -> Int{
-    return NSCalendar.currentCalendar().components(NSCalendarUnit.CalendarUnitDay, fromDate: date, toDate: self, options: nil).day
-  }
-  func hoursFrom(date:NSDate) -> Int{
-    return NSCalendar.currentCalendar().components(NSCalendarUnit.CalendarUnitHour, fromDate: date, toDate: self, options: nil).hour
-  }
-  func minutesFrom(date:NSDate) -> Int{
-    return NSCalendar.currentCalendar().components(NSCalendarUnit.CalendarUnitMinute, fromDate: date, toDate: self, options: nil).minute
-  }
-  func secondsFrom(date:NSDate) -> Int{
-    return NSCalendar.currentCalendar().components(NSCalendarUnit.CalendarUnitSecond, fromDate: date, toDate: self, options: nil).second
-  }
+// MARK: Extension of Date
+private extension Date {
+    
+    func yearsFrom(date: Date) -> Int {
+        return Calendar.current.dateComponents([.year], from: date, to: self).year ?? 0
+    }
+    
+    func monthsFrom(date:Date) -> Int {
+        return Calendar.current.dateComponents([.month], from: date, to: self).month ?? 0
+    }
+    
+    func weeksFrom(date:Date) -> Int {
+        return Calendar.current.dateComponents([.weekOfYear], from: date, to: self).weekOfYear ?? 0
+    }
+    
+    func daysFrom(date:Date) -> Int {
+        return Calendar.current.dateComponents([.day], from: date, to: self).day ?? 0
+    }
+    
+    func hoursFrom(date:Date) -> Int {
+        return Calendar.current.dateComponents([.hour], from: date, to: self).hour ?? 0
+    }
+    
+    func minutesFrom(date:Date) -> Int {
+        return Calendar.current.dateComponents([.minute], from: date, to: self).minute ?? 0
+    }
+    
+    func secondsFrom(date:Date) -> Int {
+        return Calendar.current.dateComponents([.second], from: date, to: self).second ?? 0
+    }
 }
